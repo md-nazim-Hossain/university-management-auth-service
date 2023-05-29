6ee3f287cbcc5d768d5a93bec491fcdd07dae077
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2023-05-29 23:36:12 +0600

    [done] create users service and routes

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose, { Error } from 'mongoose'
-import app from './app'
-import config from './config/index'
+import mongoose, { Error } from 'mongoose';
+import app from './app';
+import config from './config/index';
 async function main() {
   try {
-    await mongoose.connect(config.db_url as string)
-    console.log('Database connected successfully!')
+    await mongoose.connect(config.db_url as string);
+    console.log('Database connected successfully!');
     app.listen(config.port, () => {
-      console.log(`Server app listening on port ${config.port}`)
-    })
+      console.log(`Server app listening on port ${config.port}`);
+    });
   } catch (error: Error | unknown) {
     console.log(
       'Failed To connected database',
       error instanceof Error ? error.message : error
-    )
+    );
   }
 }
 //
-main()
+main();
